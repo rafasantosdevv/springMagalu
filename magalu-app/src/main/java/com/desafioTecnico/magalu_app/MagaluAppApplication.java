b7b1a53597fc5f8b75ee3ce27973097f23b8da63
Rafael <rafaeldosantos__@outlook.com>
2026-05-31 12:20:15 -0300

    Initial commit

---
+++ magalu-app/src/main/java/com/desafioTecnico/magalu_app/MagaluAppApplication.java
@@ -0,0 +1,13 @@
+package com.desafioTecnico.magalu_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluAppApplication.class, args);
+	}
+
+}
---
+++ magalu-app/src/test/java/com/desafioTecnico/magalu_app/MagaluAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioTecnico.magalu_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
